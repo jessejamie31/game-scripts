2e6463a9c8b55cbeece5b79a7a1e7f82207f57a1
Unknown <jessejohnroberts@gmail.com>
2016-09-12 13:44:44 -0600

    first unity commit

---
+++ StarCatcher/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed = 10;
+    //Use to move the player
+    private CharacterController controller;
+    //Use to assign location as a temp var
+    private Vector3 tempPosition;
+    public float gravity = -9.81f;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Fake gravity
+        tempPosition.y = gravity;
+        //Using Axis as an imput with speed the character can hold or move left or right
+        tempPosition.x = speed*Input.GetAxis("Horizontal");
+        tempPosition *= Time.deltaTime;
+        //Move method take the temp vector3 to move
+        controller.Move(tempPosition);
+	}
+}
---
+++ StarCatcher/Assets/slide.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slide : MonoBehaviour {
+
+    private CharacterController myCC;
+    public int slideDuration = 20;
+    public float slideTime = 0.01f;
+	
+    IEnumerator Slide ()
+    {
+        int i = 0;
+        while (i < slideDuration)
+        {
+            i++;
+            yield return new WaitForSeconds(slideTime);
+            print("sliding");
+        }
+        //While iterations
+        //Wait for secs
+        //Move the character controller
+    }
+
+	void Start () {
+        myCC = GetComponent<CharacterController>();
+        StartCoroutine(Slide());
+	
+	}
+	
+	
+}

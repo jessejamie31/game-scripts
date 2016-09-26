b242ae6a99dfd6566d61f06b61871258910c011b
jessejamie31 <jessejohnroberts@gmail.com>
2016-09-26 12:09:05 -0600

    worked on inheritance script

---
+++ Assets/dowhileloop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dowhileloop : MonoBehaviour {
+
+	int badguys = 3;
+
+	// Use this for initialization
+	void Start () 
+	{
+		while(badguys > 0);
+		{
+			Debug.Log ("Enemy Terminated");
+			badguys--;
+		}
+	
+	
+	}
+}
---
+++ Assets/inheritance.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inheritance : MonoBehaviour {
+
+	public class Candybar
+	{
+		public string color;
+
+	// Use this for initialization
+		public Candybar() 
+		{
+			color = "red";
+			Debug.Log("1st Candybar Constructor Called");
+	}
+		public Candybar(string newColor)
+		{
+			color = newColor;
+			Debug.Log("2nd Candybar Constructor Called");
+	
+		}
+
+		public void Eat()
+	{
+		Debug.Log ("The " + color + " candybar has been eaten.");
+	}
+		public void SayHello()
+		{
+			Debug.Log ("Hello, I am a candybar.");
+		}
+}
+}

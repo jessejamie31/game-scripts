4a21de60b4b27726ddd91b116fe199aba46ac9d9
jessejamie31 <jessejohnroberts@gmail.com>
2016-11-07 21:18:57 -0700

    workde on loop script

---
+++ Assets/loop.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loop : MonoBehaviour {
+
+	int numCandybars = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+		for (int i = 0; i < numCandybars; i++) {
+			Debug.Log ("Creating candbar number: " + i);
+		}
+	}
+}

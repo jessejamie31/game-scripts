cf934949909a2837389a9d1d3fa9767be65f7daf
jessejamie31 <jessejohnroberts@gmail.com>
2016-11-07 20:13:56 -0700

    Scope and access modifier scripts

---
+++ Assets/AnotherClass.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass : MonoBehaviour {
+
+	public int batman;
+	public int superman;
+
+	private int joker;
+
+	public void DCheroes (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log ("Hero total: " + answer);
+	}
+
+}
